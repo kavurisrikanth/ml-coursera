e281613fd96b22f951d062d708c02457ae4b3f1d
Srikanth <srikanthksr@d3e.studio>
2019-09-23 18:36:00 +0530

    continued linear regression

--- algorithm-impl/linear-regression.py
+++ algorithm-impl/linear-regression.py
@@ -1,9 +1,15 @@
+import os
+import csv
+import random
+
 def hypothesis(theta: list, X: list) -> float:
     '''
     Returns the hypothesis function result for the given theta and X vectors.
     '''
     m = len(theta)
     ans = 0.0
+    # print(theta)
+    # print(X)
 
     for i in range(m):
         ans += theta[i] * X[i]
@@ -28,7 +34,8 @@ def apply(X: list, y: list) -> list:
     min_cost = float('inf')
 
     # Add bias
-    X = [x.append(0, 1) for x in X]
+    for x in X:
+        x.insert(0, 1)
     for i in range(m):
         hypo[i] = hypothesis(theta, X[i])
 
@@ -48,3 +55,64 @@ def apply(X: list, y: list) -> list:
             theta[j] = theta[j] - ((alpha/m) * gradient)
 
     return min_theta
+
+def predict(X: list, y: list, theta: list):
+    m = len(X)
+    hypo = [0] * m
+
+    # Add bias
+    for x in X:
+        x.insert(0, 1)
+    for i in range(m):
+        hypo[i] = hypothesis(theta, X[i])
+
+    path = os.path.join(os.path.join(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'datasets'), 'redwine'), 'winequality-red.csv')
+    with open(path, mode='w') as f:
+        for i in range(m):
+            f.write('Predicted: ' + str(hypo[i]) + ', Actual: ' + y[i] + ', Error: ' + str(y[i] - hypo[i]))
+
+def run(path: str, split=0.7):
+    X = []
+    y = []
+    data = []
+    skip = True
+
+    with open(path, newline='') as f:
+        reader = csv.reader(f)
+
+        #with open(os.path.join(os.path.dirname(path), 'debug.txt'), mode='w') as d:
+        for row in reader:
+            if skip:
+                skip = False
+                continue 
+            data.append(row)
+                # d.write(str(row))
+
+    train_x, train_y, test_x, test_y = split_data(data, split)
+
+    theta = apply(train_x, train_y)
+    predict(test_x, test_y, theta)
+
+
+def split_data(data: list, split=0.7) -> tuple:
+    random.shuffle(data)
+    cut = int(len(data) * split)
+
+    train = data[:cut]
+    test = data[cut:]
+
+    train_y = [t.pop() for t in train]
+    train_x = train
+
+    test_y = [t.pop() for t in test]
+    test_x = test
+
+    return (train_x, train_y, test_x, test_y)
+
+if __name__ == '__main__':
+    dirname = os.path.dirname(os.path.dirname(__file__))
+    datasets = os.path.join(dirname, 'datasets')
+    redwine = os.path.join(os.path.join(datasets, 'redwine'), 'winequality-red.csv')
+    
+    # print(os.getcwd())
+    run(redwine)
